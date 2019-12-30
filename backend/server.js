f28bc835061cb177d93655a6d0b088b40ca77a26
Juminhark <40381241+Juminhark@users.noreply.github.com>
2019-12-30 16:35:21 +0900

    create backend

--- backend/server.js
+++ backend/server.js
@@ -5,16 +5,17 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true,useUnifiedTopology: true }
+);
 const connection = mongoose.connection;
 connection.once('open', () => {
-    console.log("MongoDB database connection established successfully");
+  console.log("MongoDB database connection established successfully");
 })
 
 const exercisesRouter = require('./routes/exercises');
