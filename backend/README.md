ee62a443c418ad7e6b971d95c59f4d000fabeb91
Juminhark <40381241+Juminhark@users.noreply.github.com>
2020-04-02 18:29:28 +0900

    audit fix

--- backend/README.md
+++ backend/README.md
@@ -13,3 +13,27 @@ npm i express cors mongoose dotenv
 * [`cors`](https://www.npmjs.com/package/cors) : [`교차 출처 리소스 공유`](https://developer.mozilla.org/ko/docs/Web/HTTP/CORS)
 * [`mongoose`](https://www.npmjs.com/package/mongoose)
 * [`dotenv`](https://www.npmjs.com/package/dotenv) : .env를 통해 key들을 관리하고 외부로의 노출을 차단
+
+## devDependencies install
+
+```sh
+npm i -D nodemon
+```
+
+* [`nodemon`](https://nodemon.io/) : Utility that will monitor for any changes in your source and automatically restart your serve
+
+```ts
+  "scripts": {
+    "start": "nodemon server.js"
+  },
+```
+
+## router
+server.js
+```ts
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
+```
+* 각 uri에 route는 해당 router를 실행.
+
+## [`rest api`](https://zetawiki.com/wiki/REST_API)
--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,35 @@
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
 const express = require('express');
-const cors = require('cors');
 const mongoose = require('mongoose');
-
+const cors = require('cors');
 require('dotenv').config();
 
-const app = express();
 const port = process.env.PORT;
+const uri = process.env.ATLAS_URI;
 
+const app = express();
 app.use(cors());
 app.use(express.json());
 
 // DB 연결
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true,useUnifiedTopology: true }
+mongoose.connect(uri, 
+  { 
+    useNewUrlParser: true, 
+    useCreateIndex: true,
+    useUnifiedTopology: true 
+  }
 );
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
 })
 
-const exercisesRouter = require('./routes/exercises');
-const usersRouter = require('./routes/users');
+
 
 app.use('/exercises', exercisesRouter);
 app.use('/users', usersRouter);
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on port : ${port}`);
 });
